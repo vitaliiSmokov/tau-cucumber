41764e25d53a109cca12ecedfc4a1a9d38ff0743
Vitalii Smokov <vitalii.smokov@globaltechmakers.com>
2020-06-24 17:46:48 +0300

    Add steps

---
+++ src/main/java/steps/LoginSteps.java
@@ -0,0 +1,22 @@
+package steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+    @Given("I am in the login page of the Para Bank application")
+    public void iAmInTheLoginPageOfTheParaBankApplication() {
+
+    }
+
+    @When("I enter valid credentials")
+    public void iEnterValidCredentials() {
+
+    }
+
+    @Then("I should be taken to the Overview page")
+    public void iShouldBeTakenToTheOverviewPage() {
+
+    }
+}
